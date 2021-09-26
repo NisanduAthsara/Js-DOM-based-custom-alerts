877e605dad1c05e1479680b12064f5ce2adebcf4
Nisandu Athsara <88542400+NisanduAthsara@users.noreply.github.com>
2021-09-26 20:48:54 +0530

    Add files

---
+++ Custom Alerts/script.js
@@ -0,0 +1,21 @@
+
+
+function myfunc(){
+	var x = document.querySelector(".alert");
+	x.style.display = "none";
+}
+
+function myfunc1(){
+	var x = document.querySelector(".sample-msg");
+	x.style.display = "none";
+}
+
+function myfunc2(){
+	var x = document.querySelector(".info-msg");
+	x.style.display = "none";
+}
+
+function myfunc3(){
+	var x = document.querySelector(".success");
+	x.style.display = "none";
+}
